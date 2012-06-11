0ce73c0bb20c34cc613f898523c8b774bda2c925
Pedro Kostelec <pedrokost@gmail.com>
2012-06-11 22:03:37 +0200

    Use current directory as file path instead of hardcoded value

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+scriptdir="`pwd`"
+
 echo "initializing submodules"
 git submodule init
 git submodule update
@@ -15,14 +17,15 @@ rm ~/.bash_profile
 rm ~/.oh-my-zsh/themes/myusuf3.zsh-theme
 
 echo "Symlinking files"
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/zshrc ~/.zshrc
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/bashrc ~/.bashrc
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/gitignore ~/.gitignore
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/tmux ~/.tmux.conf
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/gemrc ~/.gemrc
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/Documents/Dropbox/Misc/dev/dotfiles/myusuf3.zsh-theme ~/.oh-my-zsh/themes/myusuf3.zsh-theme
+# TODO: auto-detect cwd
+ln -s $scriptdir/zshrc ~/.zshrc
+ln -s $scriptdir/bashrc ~/.bashrc
+ln -s $scriptdir/gitconfig ~/.gitconfig
+ln -s $scriptdir/gitignore ~/.gitignore
+ln -s $scriptdir/tmux ~/.tmux.conf
+ln -s $scriptdir/gemrc ~/.gemrc
+ln -s $scriptdir/bash_profile ~/.bash_profile
+ln -s $scriptdir/myusuf3.zsh-theme ~/.oh-my-zsh/themes/myusuf3.zsh-theme
 
 echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
