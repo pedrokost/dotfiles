94fcb32419c3b0b7e2aa4efe30fa435d93d9f242
Pedro Kostelec <pedrokost@gmail.com>
2015-06-12 19:12:47 +0200

    Script to return approx working time

--- isxlab.sh
+++ isxlab.sh
@@ -4,10 +4,10 @@ start_time=7
 end_time=17
 
 isweekday () {
-	if [[ $(date +%u) -lt 6 ]] ; then
-		return 1
-	else
+	if [ $(date +%u) -lt 6 ] ; then
 		return 0
+	else
+		return 1
 	fi
 }
 
@@ -15,18 +15,20 @@ isworkinghour () {
 	TIME=$(date +%H)
 
 	if [ $TIME -ge "$start_time" ] &&  [ $TIME -lt "$end_time" ]; then
-		return 1
-	else
 		return 0
+	else
+		return 1
 	fi
 }
 
 isxlab () {
-	if [[ `hostname -s` = $xlab_hostname ]]; then
-		return 1
-	else
+	if [ `hostname -s` = $xlab_hostname ]; then
 		return 0
+	else
+		return 1
 	fi
 }
 
+# 0 means true, 1 means false
 (isxlab && isweekday && isworkinghour)
+
