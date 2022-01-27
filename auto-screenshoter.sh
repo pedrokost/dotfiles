b1058fd6783be502ef0a90830bfe9c151e4e223b
Pedro Kostelec <pedrokost@gmail.com>
2022-01-27 21:18:50 +0100

    [FIX] Correct directory

--- auto-screenshoter.sh
+++ auto-screenshoter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-directory="/home/$USER/Pictures/auto-screenshots/$(date -u +%Y-%V)/$(date -u +%Y-%m-%d)"
+directory="$HOME/Pictures/auto-screenshots/$(date -u +%Y-%V)/$(date -u +%Y-%m-%d)"
 mkdir -p "$directory"
 filename="$(date -u --iso-8601=seconds).png"
 import -window root -resize 640 -filter Gaussian -define filter:sigma=1  "$directory/$filename"
